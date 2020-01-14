38a5d333bf59b1d36e3fa78d947ed95a7d214892
Chayan Martinez <nayachmartinez68@gmail.com>
2020-01-14 10:20:19 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -7,9 +7,10 @@ class School
     @roster = {}
 end
   
-  def add_student(student, grade)
+  def add_student(student, number)
      roster[grade] ||= []
      roster[grade] << student
+  end
   
   
   
